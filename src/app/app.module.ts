81d783ac729e025458dc39e17302207b3dcab2c9
havababay <35009186+havababay@users.noreply.github.com>
2022-09-24 11:55:21 +0300

    import charts 2.2.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, GoogleChartsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
