3fd419fc9c9e3ffd3848fb7d75c66434ca907d2f
havababay <35009186+havababay@users.noreply.github.com>
2022-09-30 10:46:46 +0300

    bar chart with data

--- src/app/reads-by-age/reads-by-age.component.ts
+++ src/app/reads-by-age/reads-by-age.component.ts
@@ -16,22 +16,20 @@ export class ReadsByAgeComponent implements OnInit {
     // Bar chart
     bType = ChartType.ColumnChart;
     bData = [
-      ['other', 16, 'color: rgb(143, 27, 0)', '$6'],
-      ['Architect', 3, 'color: rgb(143, 27, 0)', 3],
-      ['Business', 2, 'color: rgb(143, 27, 0)', 2],
-      ['Project', 5, 'color: rgb(143, 27, 0)', 4],
-      ['developer', 9, 'color: rgb(143, 27, 0)', 4],
+      ['18-24', 160, 'blue', 160],
+      ['25-34', 30, 'blue', 30],
+      ['35-44', 20, 'blue', 20],
+      ['45-54', 50, 'blue', 50],
+      ['55-64', 90, 'blue', 90],
+      ['65+', 90, 'blue', 90],
     ];
-    bColumnNames = ['Year', 'value', { role: 'style' }, { role: 'annotation' }];
+    bColumnNames = ['Age', 'Books Read', { role: 'style' }, { role: 'annotation' }];
     bOptions = {
       title: '',
       /*'width':400,*/ height: 200,
       tooltip: {
-        // isHtml: false,
-        // trigger: "focus",
-        textStyle: { color: 'blue', fontName: 'Tahoma', fontSize: '15' },
+        textStyle: { color: 'black', fontName: 'Tahoma', fontSize: '15' },
       },
-      //                        slices: [{color: '0cac64'}, {color: 'e9ad2f'}, {color: '1f6882'},  {color: 'ff4d4d'}, {color: '9e70ff'}],
       labels: 'none',
       pieSliceText: 'none',
       pieSliceTextStyle: { color: 'red', fontSize: 9, display: 'none' },
