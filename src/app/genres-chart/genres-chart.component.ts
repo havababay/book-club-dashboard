afa40652384477a918decd922bc60c54e12b9ac4
havababay <35009186+havababay@users.noreply.github.com>
2022-10-07 17:31:43 +0300

    genres chart to get url parameters

--- src/app/genres-chart/genres-chart.component.ts
+++ src/app/genres-chart/genres-chart.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from 'angular-google-charts';
+import { ActivatedRoute } from '@angular/router';
+import { AgeRange } from '../../assets/data/report-metadata';
 
 @Component({
   selector: 'app-genres-chart',
   templateUrl: './genres-chart.component.html',
-  styleUrls: ['./genres-chart.component.css']
+  styleUrls: ['./genres-chart.component.css'],
 })
 export class GenresChartComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
+  currentAgeRange: AgeRange;
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.currentAgeRange = params['dateRange'];
+    });
   }
-    // pie
-    pType = ChartType.PieChart;
-    pCols = ['Value', 'Count'];
-    pOpts = {
-      legend: 'dsd',
-      chartArea: { width: '100%' },
-      width: 800, height: 300,
-    };
-    pData = [
-      ['Romance', 1],
-      ['Young adult', 2],
-      ['Historical fiction', 3],
-      ['Science fiction', 3],
-      ['Autobiography', 3],
-    ];
+
+  // pie
+  pType = ChartType.PieChart;
+  pCols = ['Value', 'Count'];
+  pOpts = {
+    legend: 'dsd',
+    chartArea: { width: '100%' },
+    width: 800,
+    height: 300,
+  };
+  pData = [
+    ['Romance', 1],
+    ['Young adult', 2],
+    ['Historical fiction', 3],
+    ['Science fiction', 3],
+    ['Autobiography', 3],
+  ];
 }
