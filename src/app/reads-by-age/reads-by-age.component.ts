de7e42f0912ad92df55a85c665ea882b80e99720
havababay <35009186+havababay@users.noreply.github.com>
2022-10-08 07:44:37 +0300

    material progress

--- src/app/reads-by-age/reads-by-age.component.ts
+++ src/app/reads-by-age/reads-by-age.component.ts
@@ -4,46 +4,51 @@ import { ChartType } from 'angular-google-charts';
 @Component({
   selector: 'app-reads-by-age',
   templateUrl: './reads-by-age.component.html',
-  styleUrls: ['./reads-by-age.component.css']
+  styleUrls: ['./reads-by-age.component.css'],
 })
 export class ReadsByAgeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  isLoading = false;
 
-    // Bar chart
-    bType = ChartType.ColumnChart;
-    bData = [
-      ['18-24', 160, 'blue', 160],
-      ['25-34', 30, 'blue', 30],
-      ['35-44', 20, 'blue', 20],
-      ['45-54', 50, 'blue', 50],
-      ['55-64', 90, 'blue', 90],
-      ['65+', 90, 'blue', 90],
-    ];
-    bColumnNames = ['Age', 'Books Read', { role: 'style' }, { role: 'annotation' }];
-    bOptions = {
-      title: '',
-      width: 800, height: 300,
-      tooltip: {
-        textStyle: { color: 'black', fontName: 'Tahoma', fontSize: '15' },
-      },
-      labels: 'none',
-      pieSliceText: 'none',
-      pieSliceTextStyle: { color: 'red', fontSize: 9, display: 'none' },
-      slices: [
-        { color: '3eafe0' },
-        { color: 'bd6a67' },
-        { color: 'b9b262' },
-        { color: '6abf7e' },
-        { color: '6c95b7' },
-      ],
-      // chartArea: { top: 10, left: 20, width: "70%", height: "75%" },
-      is3D: false /*sliceVisibilityThreshold: 0,*/,
-      fontSize: 9,
-      legend: 'dsd',
-    };
-
+  // Bar chart
+  bType = ChartType.ColumnChart;
+  bData = [
+    ['18-24', 160, 'blue', 160],
+    ['25-34', 30, 'blue', 30],
+    ['35-44', 20, 'blue', 20],
+    ['45-54', 50, 'blue', 50],
+    ['55-64', 90, 'blue', 90],
+    ['65+', 90, 'blue', 90],
+  ];
+  bColumnNames = [
+    'Age',
+    'Books Read',
+    { role: 'style' },
+    { role: 'annotation' },
+  ];
+  bOptions = {
+    title: '',
+    width: 800,
+    height: 300,
+    tooltip: {
+      textStyle: { color: 'black', fontName: 'Tahoma', fontSize: '15' },
+    },
+    labels: 'none',
+    pieSliceText: 'none',
+    pieSliceTextStyle: { color: 'red', fontSize: 9, display: 'none' },
+    slices: [
+      { color: '3eafe0' },
+      { color: 'bd6a67' },
+      { color: 'b9b262' },
+      { color: '6abf7e' },
+      { color: '6c95b7' },
+    ],
+    // chartArea: { top: 10, left: 20, width: "70%", height: "75%" },
+    is3D: false /*sliceVisibilityThreshold: 0,*/,
+    fontSize: 9,
+    legend: 'dsd',
+  };
 }
