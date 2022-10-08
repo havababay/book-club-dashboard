514b98f9927357f550c39ebf3b1b1dc64a21093f
havababay <35009186+havababay@users.noreply.github.com>
2022-10-08 16:31:20 +0300

    selectors

--- src/app/state/selectors.ts
+++ src/app/state/selectors.ts
@@ -1,10 +1,24 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { FiltersState } from './store';
+import { AppState, FiltersState, ReadsByAgeState } from './store';
 
 export const getFiltersState =
   createFeatureSelector<FiltersState>('filtersState');
-
 export const getFilters = createSelector(
   getFiltersState,
   (state) => state.filters
+);
+
+export const getReadsByAgeState =
+  createFeatureSelector<ReadsByAgeState>('readsByAgeState');
+export const getReadsByAgeData = createSelector(
+  getReadsByAgeState,
+  (state) => state.reads.data
+);
+export const getReadsByAgeError = createSelector(
+  getReadsByAgeState,
+  (state) => state.reads.error
+);
+export const isReadsByAgeLoading = createSelector(
+  getReadsByAgeState,
+  (state) => state.reads.loading
 );
