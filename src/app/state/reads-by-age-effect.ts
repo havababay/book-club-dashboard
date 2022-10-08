6c63f4c3d0b4db51ef206f5c4e7b49d4b48cb775
havababay <35009186+havababay@users.noreply.github.com>
2022-10-08 12:19:39 +0300

    effect...

--- src/app/state/reads-by-age-effect.ts
+++ src/app/state/reads-by-age-effect.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
-import { mergeMap, map, catchError, tap, } from 'rxjs/operators';
+import {
+  mergeMap,
+  map,
+  catchError,
+  tap,
+  switchMap,
+  delay,
+} from 'rxjs/operators';
 import { DashboardMetricsService } from '../services/dashboard_metrics.service';
 import {
   fetchReadsByAgPending,
@@ -49,16 +56,36 @@ export class ReadsByAgeEffect {
       )
     );
 */
-  getMockDataEffect11$ = createEffect(() =>
-    this.actions$.pipe(ofType(fetchReadsByAgPending)),
-    mergeMap((action) => {
-      //console.log('new fetchReadsByAge running')
-      return this.service.fetchReadsByAge(filters : action.filters).pipe(
-        map(res => fetchReadsByAgeSuccess({ data: res })),
-        catchError(error => of(fetchReadsByAgeError({ error : error }))),
-        tap(() => { console.log('fetchReadsByAge Finished') })
+
+  getMockDataEffect11$ = createEffect(
+    () => this.actions$.pipe(ofType(fetchReadsByAgPending)),
+    tap(() => {
+      console.log('new fetchReadsByAge occurred in queue');
+    }),
+    /*switchMap((action) => 
+      this.service.fetchReadsByAge(action.filters)/*.pipe(
+        delay(3000),
+        map((res) => fetchReadsByAgeSuccess({ data: res })),
+        catchError((error) => of(fetchReadsByAgeError({ error: error })))
       )
-    }
+    )*/
+  );
+
+  getMockDataEffect12$ = createEffect(
+    () => this.actions$.pipe(ofType(fetchReadsByAgPending)),
+    tap(() => {
+      console.log('new fetchReadsByAge occurred in queue');
+    }),
+    mergeMap((action) => {
+      console.log('new fetchReadsByAge running');
+      return this.service.fetchReadsByAge(action.filters).pipe(
+        map((res) => fetchReadsByAgeSuccess({ data: res })),
+        catchError((error) => of(fetchReadsByAgeError({ error: error }))),
+        tap(() => {
+          console.log('fetchReadsByAge Finished');
+        })
+      );
+    })
   );
 
   /*Observable<Action> = this.actions$.pipe(
