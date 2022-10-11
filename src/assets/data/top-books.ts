4994e1980ea72841e1207b45fd3064a212b82227
havababay <35009186+havababay@users.noreply.github.com>
2022-10-11 09:03:06 +0300

    add padding left & right, sort table

--- src/assets/data/top-books.ts
+++ src/assets/data/top-books.ts
@@ -5,10 +5,10 @@ interface TopBook {
 }
 
 export const topBooks : TopBook[] = [
-  {name: "Before I Fall", author: "Lauren Oliver", reads: 100},
-  {name: "Eleanor Oliphant Is Completely Fine", author: "Gail Honeyman", reads: 30},
-  {name: "We Were the Lucky Ones", author: "Georgia Hunter", reads: 50},
-  {name: "A Prisoner of Birth", author: "Jeffrey Archer", reads: 29},
   {name: "Never Let Me Go", author: "Kazuo Ishiguro", reads: 300},
-  {name: "Fahrenheit 451", author: "Ray Bradbury", reads: 0},
+  {name: "Before I Fall", author: "Lauren Oliver", reads: 100},
+  {name: "We Were the Lucky Ones", author: "Georgia Hunter", reads: 50},
+  {name: "Eleanor Oliphant Is Completely Fine", author: "Gail Honeyman", reads: 30},
+  {name: "A Prisoner of Birth", author: "Jeffrey Archer", reads: 29},
+  {name: "Fahrenheit 451", author: "Ray Bradbury", reads: 20},
 ];
